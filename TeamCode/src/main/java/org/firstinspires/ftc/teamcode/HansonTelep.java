0f962af58c10dbe3e8661d1481d1f2bfdcb9c9ad
Gerrit <gjkaup@gmail.com>
2019-12-06 17:23:20 -0700

    Hanson epic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HansonTelep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HansonTelep.java
@@ -14,10 +14,15 @@ public class HansonTelep extends LinearOpMode {
     public void mecanumDrive(double leftStickY, double leftStickX, double rightStickX,
                              boolean incSpeed, boolean decSpeed) {
         int noSpeed=0;
-        double Speed = .5;
-        float leftY;
-        float leftX;
-        double rightX = .5;
+        double Speed = .4;
+        double rightRotX = .5;
+
+        if ((gamepad1.dpad_up = true) && Speed < 1) {
+            Speed = Speed + .2;
+        }
+        if ((gamepad1.dpad_down = true) && Speed > 0) {
+            Speed = Speed - .2;
+        }
         if (leftStickY >= robot.STICK_THRES || leftStickY <= -robot.STICK_THRES) {
             robot.RightFront.setPower(Speed * leftStickY);
             robot.LeftFront.setPower(Speed * leftStickY);
@@ -31,16 +36,10 @@ public class HansonTelep extends LinearOpMode {
             robot.RightBack.setPower(-Speed * leftStickX);
         }
         if (rightStickX >= robot.STICK_THRES || rightStickX <= -robot.STICK_THRES) {
-            robot.LeftFront.setPower(rightX);
-            robot.RightFront.setPower(-rightX);
-            robot.LeftBack.setPower(rightX);
-            robot.RightBack.setPower(-rightX);
-        }
-        if (gamepad1.dpad_up && Speed < 1) {
-            Speed = Speed + .1;
-        }
-        if (gamepad1.dpad_down && Speed > 0) {
-            Speed = Speed + .1;
+            robot.LeftFront.setPower(rightRotX);
+            robot.RightFront.setPower(-rightRotX);
+            robot.LeftBack.setPower(rightRotX);
+            robot.RightBack.setPower(-rightRotX);
         }
         else {
             robot.LeftFront.setPower(noSpeed);
@@ -48,12 +47,19 @@ public class HansonTelep extends LinearOpMode {
             robot.LeftBack.setPower(noSpeed);
             robot.RightBack.setPower(noSpeed);
         }
-        if (gamepad1.dpad_up && Speed < 1) {
-            Speed = Speed + .1;
-        }
-        if (gamepad1.dpad_down && Speed > 0) {
-            Speed = Speed + .1;
-        }
+
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double RightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + RightX;
+        final double v2 = r * Math.sin(robotAngle) - RightX;
+        final double v3 = r * Math.sin(robotAngle) + RightX;
+        final double v4 = r * Math.cos(robotAngle) - RightX;
+
+        robot.LeftFront.setPower(v1);
+        robot.RightFront.setPower(v2);
+        robot.LeftBack.setPower(v3);
+        robot.RightBack.setPower(v4);
     // Turn this into a double and put it into mecanumDrive's input (be sure to define it before
     // mecanumDrive though
 
