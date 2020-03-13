c2ff3724f85664f1c78fd7917ff1ca77597bb5b2
rswart0604 <rswart0604@gmail.com>
2020-03-12 17:20:29 -0700

    new

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisTeleop.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.motion.MecanumDrive;
+
+@TeleOp(name = "Chassis Teleop", group = "Robot")
+public class ChassisTeleop extends LinearOpMode {
+
+    MecanumDrive mecanum = new MecanumDrive();
+
+
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
