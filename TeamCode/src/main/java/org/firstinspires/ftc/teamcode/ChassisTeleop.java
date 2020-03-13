1e34b8e0cf72a3a3816e2aecbe3e6564a3190182
rswart0604 <rswart0604@gmail.com>
2020-03-12 21:37:21 -0700

    chassis teleop done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisTeleop.java
@@ -8,13 +8,37 @@ import org.firstinspires.ftc.teamcode.motion.MecanumDrive;
 @TeleOp(name = "Chassis Teleop", group = "Robot")
 public class ChassisTeleop extends LinearOpMode {
 
-    MecanumDrive mecanum = new MecanumDrive();
-
-
-
+    RobotHardware robot = new RobotHardware(false);
+    MecanumDrive mecanum_drive = new MecanumDrive();
+    boolean speedUp = true;
 
     @Override
     public void runOpMode() {
 
+        robot.initMecanum(hardwareMap, telemetry);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.a) {
+                speedUp = true;
+            } else if (gamepad1.b) {
+                speedUp = false;
+            }
+
+            if (!speedUp) {
+                mecanum_drive.mecanumDrive(
+                        telemetry, robot,
+                        gamepad1.left_stick_y, gamepad1.left_stick_x,
+                        gamepad1.right_stick_x,
+                        gamepad1.dpad_up, gamepad1.dpad_right, gamepad1.dpad_down, gamepad1.dpad_left);
+            } else {
+                mecanum_drive.mecanumDriveFast(telemetry, robot,
+                        gamepad1.left_stick_y, gamepad1.left_stick_x,
+                        gamepad1.right_stick_x,
+                        gamepad1.dpad_up, gamepad1.dpad_right, gamepad1.dpad_down, gamepad1.dpad_left);
+            }
+        }
     }
 }
